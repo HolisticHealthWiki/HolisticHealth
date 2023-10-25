7cd90673cfdb984320d90ee9029bf6dc4aa4d145
Vivek Joshy <8206808+vivekjoshy@users.noreply.github.com>
2023-10-25 23:36:31 +0530

    Add type hints for agents

--- HolisticHealth/api/v1/agents.py
+++ HolisticHealth/api/v1/agents.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 from fastapi import APIRouter
 from loguru import logger
 
@@ -5,7 +7,7 @@ agents_router = APIRouter(prefix="/agents")
 
 
 @agents_router.post("/user/")
-async def user(message: str):
+async def user(message: str) -> Dict[str, str]:
     reversed_message = message[::-1]
     logger.debug(f"User Message Reversed: {reversed_message}")
     return {"String Successfully Reversed": f"{reversed_message}"}
